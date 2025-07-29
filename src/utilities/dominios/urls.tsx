01e1ebfff0e20285088affb907a73ea6a7aa017f
unknown <julian2000mk1@gmail.com>
2025-07-29 17:52:17 -0500

    Se sube ruta de despliegue backend

--- src/utilities/dominios/urls.tsx
+++ src/utilities/dominios/urls.tsx
@@ -1,5 +1,5 @@
 export const URLS = {
-    URL_BASE: "http://localhost:3550",
+    URL_BASE: "http://192.168.1.31:3550",
     INICIAR_SESION: "/public/accesos/signin",
     REGISTRO: "/public/registros/user",
     LISTAR_PUBLICACION_PUBLICA: "/public/publicaciones/publico",
